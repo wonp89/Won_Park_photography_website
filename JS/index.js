4af8251ab8b19151a78af3a08d38ac80edb1c1dd
Won <wonpark89@gmail.com>
2017-12-14 20:51:30 -0800

    modal with gototop button

---
+++ JS/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    $('#layer-one').mousemove(function (e) {
+        parallax(e, this, 1);
+        parallax(e, document.getElementById('layer-two'), 2);
+    });
+});
+
+function parallax(e, target, layer) {
+    var layer_coeff = 10 / layer;
+    var x = ($(window).width() - target.offsetWidth) / 2 - (e.pageX - ($(window).width() / 2)) / layer_coeff;
+    var y = ($(window).height() - target.offsetHeight) / 2 - (e.pageY - ($(window).height() / 2)) / layer_coeff;
+    $(target).offset({ top: y ,left : x });
+};
+
+//------------------ modal for image viewer ----------------//
+  var modal = document.getElementById('myModal');
+  var img = document.getElementsByTagName('img');
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  for (i = 0; i < img.length; i++) {
+    img[i].onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+  }
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+//------ button for go to top ----------//
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
