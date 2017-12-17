f15d62d17ddbc034f14eb286707b12887915f990
Won <wonpark89@gmail.com>
2017-12-16 16:13:35 -0800

    chaning scrolling and hover effect

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,25 @@
+//----------- Jquery ------------//
 $(document).ready(function () {
-    $('#layer-one').mousemove(function (e) {
-        parallax(e, this, 1);
-        parallax(e, document.getElementById('layer-two'), 2);
+    $('#layer').mousemove(function (e) { //first layer behind layer2, mousemove for transfering mouse event
+        parallax(e, this, 2);
     });
-    if ($('#img01').height() > 700) {
-    	$('#img01').width(467)
-    }
+    //brightness effect when scrolled down
+    $(window).scroll(function() {
+           $('img').each(function(){
+           var imagePos = $(this).offset().top;
+           var topOfWindow = $(window).scrollTop();
+               if (imagePos < topOfWindow+600) {
+                   $(this).addClass("imgBrigtness");
+               }
+           });
+       });
+    //hover take brightness effect
+    $("img").hover(function(e){
+         $(this).addClass("imgBrigtness");
+    });
 });
+
+//------ parallax : width and height calculation for layer movement ------//
 function parallax(e, target, layer) {
     var layer_coeff = 10 / layer;
     var x = ($(window).width() - target.offsetWidth) / 2 - (e.pageX - ($(window).width() / 2)) / layer_coeff;
@@ -14,7 +27,7 @@ function parallax(e, target, layer) {
     $(target).offset({ top: y ,left : x }); //transfer to jquery with values when mouse cursor moves
 };
 
-//------------------ modal for image viewer ----------------//
+//------------ modal for image viewer ----------------//
   var modal = document.getElementById('myModal');
   var img = document.getElementsByTagName('img');
   var modalImg = document.getElementById("img01");
@@ -22,6 +35,7 @@ function parallax(e, target, layer) {
     img[i].onclick = function(){
         modal.style.display = "block";
         modalImg.src = this.src;
+        // resize the horizontal images
         if (modalImg.height >= 700 || modalImg.height >= 650) {
           modalImg.style.width = "467px";
         } else {
